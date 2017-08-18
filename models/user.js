9c3cb232e5ed61a22a191e67928ee16094e88bfa
alexandraalday <alexandra.alday@gmail.com>
2017-08-18 13:34:33 -0700

    created user model and controller

---
+++ models/user.js
@@ -0,0 +1,16 @@
+//dependencies
+const mongoose = require('mongoose');
+
+//schema
+const userSchema = mongoose.Schema({
+ 	name: String, 
+ 	image: String,
+	bio: String,
+	author: []
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+//export
+module.exports = User;
